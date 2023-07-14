85038eb716426135224052c93c8d4528a76ac432
chaimaazerouali <zeroualiiichaimaa@gmail.com>
2023-07-14 13:27:29 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description :prints all single digit numbers of base 10 starting from 0
+ * Return: 0
+ */
+
+int main(void)
+{
+		char N;
+
+		for (N = 48; N <= 57; N++)
+		{
+			putchar(N);
+		}
+
+		putchar('\n');
+
+		return (0);
+}
