5959713e20b70c221657751f0262bfb9d3d3b4fe
chaimaazerouali <zeroualiiichaimaa@gmail.com>
2023-07-27 13:24:46 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,14 +8,15 @@
 */
 
 char *string_toupper(char *s)
+
 {
 	int a = 0;
 
 	while (s[a] != '\0')
 	{
-		if (s[a] >= 97 && s[a] <= 122)
-			s[a] = s[a] - 32;      
-		a++;
+	if (s[a] >= 'a' && s[a] <= 'z')
+	s[a] -= 32;      
+	a++;
 	}
 return (s);
 }
