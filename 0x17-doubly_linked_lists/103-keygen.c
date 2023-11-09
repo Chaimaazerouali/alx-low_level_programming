d0044a888a00962d0b5edbdd71fd5746fd4e7504
zerouali chaimaa <zeroualiiichaimaa@gmail.com>
2023-11-09 13:25:35 -0800

    c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 {
 unsigned int index1, value1;
 size_t username_length, accumulated_value;
-char *key_characters = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+char *k_c = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 char generated_key[7] = "      ";
 
 if (argc != 2)
@@ -20,24 +20,24 @@ printf("Correct usage: ./keygen5 username\n");
 return (1);
 }
 username_length = strlen(argv[1]);
-generated_key[0] = key_characters[(username_length ^ 59) & 63];
+generated_key[0] = k_c[(username_length ^ 59) & 63];
 for (index1 = 0, accumulated_value = 0; index1 < username_length; index1++)
 accumulated_value += argv[1][index1];
-generated_key[1] = key_characters[(accumulated_value ^ 79) & 63];
+generated_key[1] = k_c[(accumulated_value ^ 79) & 63];
 for (index1 = 0, value1 = 1; index1 < username_length; index1++)
 value1 *= argv[1][index1];
-generated_key[2] = key_characters[(value1 ^ 85) & 63];
+generated_key[2] = k_c[(value1 ^ 85) & 63];
 for (value1 = argv[1][0], index1 = 0; index1 < username_length; index1++)
 if ((char)value1 <= argv[1][index1])
 value1 = argv[1][index1];
 srand(value1 ^ 14);
-generated_key[3] = key_characters[rand() & 63];
+generated_key[3] = k_c[rand() & 63];
 for (value1 = 0, index1 = 0; index1 < username_length; index1++)
 value1 += argv[1][index1] * argv[1][index1];
-generated_key[4] = key_characters[(value1 ^ 239) & 63];
+generated_key[4] = k_c[(value1 ^ 239) & 63];
 for (value1 = 0, index1 = 0; (char)index1 < argv[1][0]; index1++)
 value1 = rand();
-generated_key[5] = key_characters[(value1 ^ 229) & 63];
+generated_key[5] = k_c[(value1 ^ 229) & 63];
 printf("%s\n", generated_key);
 return (0);
 }
