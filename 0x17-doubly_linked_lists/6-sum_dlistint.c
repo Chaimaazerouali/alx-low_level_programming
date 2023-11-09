ccfac3ee8034e719ea0a0acaebf3646c1906fae3
zerouali chaimaa <zeroualiiichaimaa@gmail.com>
2023-11-09 12:16:53 -0800

    mandatory

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - Sums the values of a doubly linked list.
- * @head: Pointer to the current head node.
- *
- * Return: The sum of values in the list.
- */
+* sum_dlistint - Sums the values of a doubly linked list.
+* @head: Pointer to the current head node.
+*
+* Return: The sum of values in the list.
+*/
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
+int sum = 0;
 
-    while (head)
-    {
-        sum += head->n;
-        head = head->next;
-    }
+while (head)
+{
+sum += head->n;
+head = head->next;
+}
 
-    return (sum);
+return (sum);
 }
 
--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,48 +1,48 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node in a given position.
- * @h: A pointer to the head of the dlistint_t list.
- * @idx: The position to insert the new node.
- * @n: The integer for the new node to contain.
- *
- * Return: If the function fails - NULL. Otherwise - the address of the new node.
- */
+* insert_dnodeint_at_index - Inserts a new node in a given position.
+* @h: A pointer to the head of the dlistint_t list.
+* @idx: The position to insert the new node.
+* @n: The integer for the new node to contain.
+*
+* Return: If the function fails - NULL. Otherwise - the address of the node.
+*/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *current = *h, *new_node;
+dlistint_t *current = *h, *new_node;
 
-    if (idx == 0)
-    {
-        return (add_dnodeint(h, n)); /* Insert at the beginning*/
-    }
+if (idx == 0)
+{
+return (add_dnodeint(h, n)); /* Insert at the beginning*/
+}
 
-    for (; idx != 1; idx--)
-    {
-        current = current->next;
-        if (current == NULL)
-        {
-            return (NULL); /* Index out of range*/
-        }
-    }
+for (; idx != 1; idx--)
+{
+current = current->next;
+if (current == NULL)
+{
+return (NULL); /* Index out of range*/
+}
+}
 
-    if (current->next == NULL)
-    {
-        return (add_dnodeint_end(h, n)); /* Insert at the end*/
-    }
+if (current->next == NULL)
+{
+return (add_dnodeint_end(h, n)); /* Insert at the end*/
+}
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-    {
-        return (NULL); /* Memory allocation failed */
-    }
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+{
+return (NULL); /* Memory allocation failed */
+}
 
-    new_node->n = n;
-    new_node->prev = current;
-    new_node->next = current->next;
-    current->next->prev = new_node;
-    current->next = new_node;
+new_node->n = n;
+new_node->prev = current;
+new_node->next = current->next;
+current->next->prev = new_node;
+current->next = new_node;
 
-    return (new_node);
+return (new_node);
 }
 
