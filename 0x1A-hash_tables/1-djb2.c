184320633ce61459b53fe76e9b1a069fa395a76e
Chaimaazerouali <zeroualiiichaimaa@gmail.com>
2023-12-01 18:08:18 -0800

    n

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -3,19 +3,18 @@
 /**
 * hash_djb2 - Implements the djb2 algorithm for generating a hash value.
 * @str: The string used to generate the hash value.
-*
 * Return: Returns the computed hash value.
 */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-unsigned long int hs;  // Changed variable name from 'hash' to 'hs'
+unsigned long int hash;
 int c;
 
-hs = 5381;
+hash = 5381;
 while ((c = *str++))
 {
-hs = ((hs << 5) + hs) + c; /* hs * 33 + c */  // Changed 'hash' to 'hs'
+hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 }
-return (hs);
+return (hash);
 }
 
