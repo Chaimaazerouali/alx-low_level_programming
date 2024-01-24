5c7aaa76969197970b9d9ee3e3e77dcdd65f26a1
Chaimaazerouali <zeroualiiichaimaaa@gmail.com>
2024-01-24 13:08:33 -0800

    alx

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
-"""Defines an island perimeter measuring function."""
-
+""" module comment """
 
 def island_perimeter(grid):
-    """Return the perimiter of an island.
+    """ return perimeter of one's in a matrix """
+width = len(grid[0])
+height = len(grid)
+count = 0
+size = 0
 
-    The grid represents water by 0 and land by 1.
+for i in range(height):
+for j in range(width):
+if grid[i][j] == 1:
+size += 1
+if (j > 0 and grid[i][j - 1] == 1):
+count += 1
+if (i > 0 and grid[i - 1][j] == 1):
+count += 1
+return size * 4 - count * 2
 
-    Args:
-        grid (list): A list of list of integers representing an island.
-    Returns:
-        The perimeter of the island defined in grid.
-    """
- cnt = 0
-    height = len(grid)
-    width = len(grid[0])
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 0:
-                if i > 0 and grid[i - 1][j] == 1:
-                        cnt += 1
-                if j > 0 and grid[i][j - 1] == 1:
-                        cnt += 1
-                if j < width - 1 and grid[i][j + 1] == 1:
-                        cnt += 1
-                if i < height - 1 and grid[i + 1][j] == 1:
-                        cnt += 1
-    return cnt
